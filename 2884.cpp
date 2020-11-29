75b8a434962746893e66c71fa17b3b5d2d8fe190
LeeBongHyeon <11jjklol@naver.com>
2020-11-29 09:33:56 +0900

    2884

---
+++ 2884.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int h, m;
+	cin >> h >> m;
+
+	if ((m - 45) < 0)
+	{
+		m = 60 - (45 - m);
+		
+		if ((h - 1) < 0)
+			h = 24 - 1;
+		else
+			h--;
+	}
+	else
+		m -= 45;
+
+	cout << h << " " << m;
+	return 0;
+}
