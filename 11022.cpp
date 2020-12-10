6f8d8f04d881373d320b9b4deec2e2ea101c0625
LeeBongHyeon <11jjklol@naver.com>
2020-12-10 10:55:36 +0900

    11022

---
+++ 11022.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int T;
+    cin >> T;
+    
+    for(int i = 0; i < T; i++)
+    {
+        int A, B;
+        cin >> A >> B;
+        cout << "Case #" << i + 1 << ": " << A << " + " << B << " = " << A + B << endl;
+    }
+    
+    return 0;
+}
