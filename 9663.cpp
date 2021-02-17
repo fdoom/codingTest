dd5c0abf0bef276524bb0fb20167d68cd7c69c47
LeeBongHyeon <11jjklol@naver.com>
2021-02-17 21:34:57 +0900

    update

--- 9663.cpp
+++ 9663.cpp
@@ -2,43 +2,41 @@
 
 using namespace std;
 
-bool board[15][15] = { 0, };
+bool board[15][15];
 int n, result = 0;
 
 bool search(int x, int y)
 {
 	for (int i = 0; i < n; i++)
 	{
-		if (board[x][i] || board[i][y]) return true; // 가로 세로 열 체크
+		if (board[i][y]) return true; // 세로 열 체크
 
-		if (x + i < n)		// 왼쪽 (위) 오른쪽(아래) 대각선 체크
-		{
-			if (y + i < n && board[x + i][y + i]) return true; 
-			if (y - i >= 0 && board[x + i][y - i]) return true;
-		}
+		// 왼쪽 (위) 오른쪽(아래) 대각선 체크
+		if (x - i >= 0 && y - i >= 0 && board[x - i][y - i]) return true;	//2사분면
 
-		//왼쪽(아래) 오른쪽(위) 대각선 체크
-		if (y + i < n && x - i >= 0 && board[x - i][y + i]) return true;
-		if (x - i >= 0 && y - i >= 0 && board[x - i][y - i]) return true;
+		// 왼쪽(아래) 오른쪽(위) 대각선 체크
+		if (x - i >= 0  && y + i < n && board[x - i][y + i]) return true;	//1사분면
 	}
 	return false;
 }
 
 void solution(int p)
 {
-	if (p == n) result++;
-	else
+	if (p == n)
 	{
-		for (int y = 0; y < n; y++)
+		result++;
+		return;
+	}
+	
+	for (int y = 0; y < n; y++)
+	{
+		if (!board[p][y])
 		{
-			if (!board[p][y])
-			{
-				if (search(p, y)) continue;
-				board[p][y] = true;
+			if (search(p, y)) continue;
+			board[p][y] = true;
 
-				solution(p + 1);
-				board[p][y] = false;
-			}
+			solution(p + 1);
+			board[p][y] = false;
 		}
 	}
 }
