8b6a46e6b5a00dece0745da99ec39daafe9616ed
LeeBongHyeon <11jjklol@naver.com>
2021-03-14 22:31:09 +0900

    2446

---
+++ 2446.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n;
+	int cnt = 0;
+	cin >> n;
+
+	cnt = n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+			cout << " ";
+
+		for (int j = 0; j < (2 * n - 1) - (2 * i); j++)
+			cout << "*";
+		cout << endl;
+	}
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 1; j < n - 1- i; j++)
+			cout << " ";
+
+		for (int j = 0; j < 3 + 2 *i; j++)
+			cout << "*";
+		cout << endl;
+	}
+	return 0;
+}
