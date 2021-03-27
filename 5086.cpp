0385684d15c2b6285f59baed5ad3c4a6230dae08
LeeBongHyeon <11jjklol@naver.com>
2021-03-27 09:31:54 +0900

    5086

---
+++ 5086.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n1, n2;
+
+	while (1)
+	{
+		cin >> n1 >> n2;
+		if (n1 == 0 && n2 == 0)
+			break;
+		else if (n2 % n1 == 0)
+			cout << "factor" << endl;
+		else if (n1 % n2 == 0)
+			cout << "multiple" << endl;
+		else
+			cout << "neither" << endl;
+	}
+	return 0;
+}
