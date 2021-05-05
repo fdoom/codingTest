53d77bae564ce2fbbec4663d9a0cc0f81b14547d
LeeBongHyeon <11jjklol@naver.com>
2021-05-05 13:41:10 +0900

    1676_modify

--- 1676.cpp
+++ 1676.cpp
@@ -12,15 +12,11 @@ int main(void)
 	for (int i = 2; i <= n; i++)
 	{
 		int temp = i;		//i의 값 임시 저장
-		while (i % 5 == 0)	//5로 나눌 수 없을 때까지 반복
+		while (temp % 5 == 0)	//5로 나눌 수 없을 때까지 반복
 		{
-			if (i % 5 == 0)
-			{
-				i /= 5;
-				cnt++;
-			}
+			temp /= 5;
+			cnt++;
 		}
-		i = temp;
 	}
 	cout << cnt;
 }
