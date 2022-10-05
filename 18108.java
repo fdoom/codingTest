21e37a1ba05111119baf1017e3a32b0bf94eee14
이봉현 <11jjklol@naver.com>
2022-10-05 16:51:54 +0900

    18108

---
+++ 18108.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        final int diff = 2541 - 1998;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int input = sc.nextInt();
+        System.out.println(input - diff);
+    }
+}
