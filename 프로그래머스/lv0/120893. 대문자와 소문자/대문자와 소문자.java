bc7fe36f6fc7aedd963cd51e46aa79d62c79fa81
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-12 19:04:55 +0900

    [level 0] Title: 대문자와 소문자, Time: 11.28 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i = 0; i < my_string.length(); i++) {
+            if(my_string.charAt(i) >= 'a')
+                answer += (char)(my_string.charAt(i) - ('a' - 'A'));
+            else
+                answer += (char)(my_string.charAt(i) + ('a' - 'A'));
+        }
+        return answer;
+    }
+}
