9859bae92092320c74b4363be212a189bfe2debe
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-12 19:21:30 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0, s).concat(overwrite_string).concat(my_string.substring(s + overwrite_string.length(), my_string.length()));
+        return answer;
+    }
+}
