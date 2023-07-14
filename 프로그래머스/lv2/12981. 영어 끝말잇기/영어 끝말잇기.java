d1c768649fd65ae565c56be5dea352903ba4da49
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-14 17:07:46 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.62 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        Stack <String> s = new Stack<>();
+        for(int i = 0; i < words.length; i++) {
+            if(i % n == 0) answer[1]++;
+            
+            if(s.isEmpty()) s.push(words[i]);
+            else if(s.indexOf(words[i]) != -1 || words[i].length() == 1 ||
+                   s.peek().charAt(s.peek().length() - 1) != words[i].charAt(0)) {
+                answer[0] = i % n + 1;
+                break;
+            }
+            else s.push(words[i]);
+            
+            if(i + 1 == words.length) answer[1] = 0;
+        }
+        return answer;
+    }
+}
