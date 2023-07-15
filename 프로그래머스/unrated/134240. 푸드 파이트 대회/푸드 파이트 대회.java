548f1eff82fefdd9f934cc0fe88a42068d1c2900
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-15 19:02:39 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 8.52 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for(int i = 1; i < food.length; i++) {
+            String num = String.valueOf(i);
+            answer += num.repeat(food[i] / 2);
+        }
+        StringBuffer sb = new StringBuffer(answer);
+        answer += "0" + sb.reverse().toString();
+        return answer;
+    }
+}
