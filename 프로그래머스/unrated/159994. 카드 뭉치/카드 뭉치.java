2c0fbbfc8a760378515bce84693cb51103ba1795
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-16 15:07:03 +0900

    [unrated] Title: 카드 뭉치, Time: 0.04 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "No";
+        List <String> c1 = new ArrayList<>(List.of(cards1));
+        List <String> c2 = new ArrayList<>(List.of(cards2));
+        for(int i = 0; i < goal.length; i++) {
+            if(c1.indexOf(goal[i]) == 0)
+                c1.remove(0);
+            else if(c2.indexOf(goal[i]) == 0)
+                c2.remove(0);
+            else
+                break;
+            if(i + 1 == goal.length)
+                answer = "Yes";
+        }
+        return answer;
+    }
+}
