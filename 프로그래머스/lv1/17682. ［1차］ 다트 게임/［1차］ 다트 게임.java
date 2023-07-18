7a5fee23d3215c7f8a202c6cf4577629f84e78a8
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-18 23:49:11 +0900

    [level 1] Title: [1차] 다트 게임, Time: 1.14 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -3,12 +3,12 @@ class Solution {
     public int solution(String dartResult) {
         int answer = 0;
         Stack <Integer> stack = new Stack<>();
-        String[] s1 = dartResult.split("[^0-9]+");
-        String[] s2 = dartResult.split("[0-9]+");
+        String[] s1 = dartResult.split("[^0-9]+");          //문자 사이에 있는 숫자로만 구성
+        String[] s2 = dartResult.split("[0-9]+");           //숫자 사이에 있는 문자로만 구성
         
         for(int i = 0; i < s1.length; i++) {
             int num = Integer.parseInt(s1[i]);
-            int index = i + 1;
+            int index = i + 1;                              //첫번째 문자열은 공백이기에 제외
             while(!s2[index].isBlank()) {
                 if(s2[index].matches("S(.*)"))
                     stack.push(num);
@@ -23,21 +23,20 @@ class Solution {
                 else if(s2[index].matches("#(.*)")) {
                     stack.push(stack.pop() * -1);
                 }
-                else {
-                    if(stack.size() >= 2) {
+                else {                                      //스타상(*)일 때
+                    if(stack.size() >= 2) {                 //이미 두 개 이상의 점수를 획득했을 때
                         int temp1 = stack.pop() * 2;
                         int temp2 = stack.pop() * 2;
                         stack.push(temp2);
                         stack.push(temp1);
                     }
-                    else {
+                    else                                    //획득한 점수가 1개일 때
                         stack.push(stack.pop() * 2);
-                    }
                 }
-                s2[index] = s2[index].substring(1);
+                s2[index] = s2[index].substring(1);         //첫 번째 문자 제거
             }
         }
-        while(!stack.isEmpty())
+        while(!stack.isEmpty())                             //계산 결과 전부 더하기
             answer += stack.pop();
         return answer;
     }
