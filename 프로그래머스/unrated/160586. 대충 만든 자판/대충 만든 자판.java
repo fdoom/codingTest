44ba12f60b136cde2d0e74c4f4ef38b4bcdd3a10
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-22 14:32:12 +0900

    [unrated] Title: 대충 만든 자판, Time: 14.87 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        for(int i = 0; i < targets.length; i++) {
+            for(char c : targets[i].toCharArray()) {
+                int press = Integer.MAX_VALUE;
+                for(String k : keymap) {
+                    for(int j = 0; j < k.length(); j++) {
+                        if(c == k.charAt(j)) {
+                            press = Math.min(press, j);
+                            break;
+                        }
+                    }
+                }
+                if(press == Integer.MAX_VALUE) {
+                    answer[i] = -1;
+                    break;
+                }
+                else answer[i] += press + 1;
+            }
+            if(answer[i] == 0) answer[i] = -1;
+        }
+        return answer;
+    }
+}
