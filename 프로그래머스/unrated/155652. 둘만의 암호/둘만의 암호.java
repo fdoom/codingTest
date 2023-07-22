00030cfb6133cc890cc2573489632775fbed6be6
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-22 22:43:38 +0900

    [unrated] Title: 둘만의 암호, Time: 22.63 ms, Memory: 96.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            int cnt = 0;
+            char temp = c;
+            for(int i = 0; i < index + cnt; i++) {
+                temp++;
+                if(temp > 'z')
+                    temp = 'a';
+                if(skip.matches("(.*)" + temp + "(.*)")) {
+                    cnt++;
+                    continue;
+                }
+            }
+            answer.append(temp);
+        }
+        return answer.toString();
+    }
+}
