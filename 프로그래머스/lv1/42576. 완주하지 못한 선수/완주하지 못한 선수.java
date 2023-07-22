f0bdcb75e884ef68cbc8992b14f6c1c31a9907ae
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-22 12:18:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 328.01 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,15 +4,12 @@ class Solution {
         String answer = "";
         Arrays.sort(participant);
         Arrays.sort(completion);
-        
         for(int i = 0; i < completion.length; i++) {
             if(!participant[i].equals(completion[i])) {
-                System.out.println(participant[i] + " " + completion[i]);
                 answer = participant[i];
                 break;
             }
         }
-        
         if(answer.isBlank())
             answer = participant[participant.length - 1];
         return answer;
