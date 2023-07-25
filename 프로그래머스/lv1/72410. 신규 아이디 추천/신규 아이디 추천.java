600f781df3c5f3679429f1a7f1a2fe99606cf7dd
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-25 10:13:22 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.62 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase().replaceAll("[^a-z|0-9|\\-|_|.]", "").replaceAll("[.]+", ".").replaceAll("^[.]|[.]$", "");
+		System.out.println(answer);
+		if(answer.isBlank())
+			answer = "aaa";
+		if(answer.length() >= 16) {
+			answer = answer.substring(0, 15);
+			answer = answer.replaceAll("^[.]|[.]$", "");
+		}
+		if(answer.length() <= 2)
+			answer += String.valueOf(answer.charAt(answer.length() - 1)).repeat(3 - answer.length());
+        return answer;
+    }
+}
