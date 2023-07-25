64f8db52516066cae6b8c29a7c45f8e1acb4a05f
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-25 10:41:51 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.59 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -3,10 +3,8 @@ class Solution {
         String answer = new_id.toLowerCase().replaceAll("[^a-z|0-9|\\-|_|.]", "").replaceAll("[.]+", ".").replaceAll("^[.]|[.]$", "");
 		if(answer.isBlank())
 			answer = "a";
-		if(answer.length() >= 16) {
-			answer = answer.substring(0, 15);
-			answer = answer.replaceAll("^[.]|[.]$", "");
-		}
+		if(answer.length() >= 16)
+			answer = answer.substring(0, 15).replaceAll("^[.]|[.]$", "");
 		if(answer.length() <= 2)
 			answer += String.valueOf(answer.charAt(answer.length() - 1)).repeat(3 - answer.length());
         return answer;
