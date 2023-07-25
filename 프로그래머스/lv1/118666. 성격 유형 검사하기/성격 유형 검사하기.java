d168f8afd22a187bc8b2d25cf34f146ad6c99e4f
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-25 17:49:28 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 2.23 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -3,10 +3,7 @@ class Solution {
     public String solution(String[] survey, int[] choices) {
         String answer = "";
         List <String> type = new ArrayList(List.of("R", "T", "C", "F", "J", "M", "A", "N"));
-        int[][] sum = new int[type.size()][2];
-        
-        for(int i = 0; i < sum.length; i++)
-            sum[i][1] = i;
+        int[] sum = new int[type.size()];
         
         for(int i = 0; i < survey.length; i++) {
             String a = survey[i].substring(0, 1);
@@ -14,16 +11,16 @@ class Solution {
             
             switch(choices[i]) {
                 case 1: case 2: case 3:
-                    sum[type.indexOf(a)][0] += 4 - choices[i];
+                    sum[type.indexOf(a)] += 4 - choices[i];
                     break;
                 case 5: case 6: case 7:
-                    sum[type.indexOf(b)][0] += choices[i] - 4;
+                    sum[type.indexOf(b)] += choices[i] - 4;
                     break;
             }
         }
         
         for(int i = 0; i < sum.length ; i += 2) {
-            if(sum[i][0] < sum[i + 1][0])
+            if(sum[i] < sum[i + 1])
                 answer += type.get(i + 1);
             else
                 answer += type.get(i);
