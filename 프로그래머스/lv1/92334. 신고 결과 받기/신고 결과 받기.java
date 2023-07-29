3535e5c2e00c746875be633db24de4f058c9f416
fdoom <44052861+fdoom@users.noreply.github.com>
2023-07-29 23:15:11 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.14 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,28 +2,28 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         Map <String, Integer> id = new HashMap<>();
-		for(int i = 0; i < id_list.length; i++)
-			id.put(id_list[i], i);
-		
-		boolean[][] count = new boolean[id_list.length][id_list.length];
-		for(String r : report) {
+        for(int i = 0; i < id_list.length; i++)
+            id.put(id_list[i], i);
+
+        boolean[][] count = new boolean[id_list.length][id_list.length];
+        for(String r : report) {
             String[] temp = r.split(" ");
             count[id.get(temp[0])][id.get(temp[1])] = true;
         }
-		
-		int[] answer = new int[id_list.length];
-		for(int i = 0; i < count.length; i++) {
-			int cnt = 0;
-			int[] temp = answer.clone();
-			for(int j = 0; j < count.length; j++) {
-				if(count[j][i]) {
-					cnt++;
-					temp[j]++;
-				}
-			}
-			if(cnt >= k)
-				answer = temp.clone();
-		}
+
+        int[] answer = new int[id_list.length];
+        for(int i = 0; i < count.length; i++) {
+            int cnt = 0;
+            int[] temp = answer.clone();
+            for(int j = 0; j < count.length; j++) {
+                if(count[j][i]) {
+                    cnt++;
+                    temp[j]++;
+                }
+            }
+            if(cnt >= k)
+                answer = temp.clone();
+        }
         return answer;
     }
 }
