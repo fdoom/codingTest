49f9cd1a954aa922e72e695802fa2a43e2c4845d
fdoom <44052861+fdoom@users.noreply.github.com>
2023-08-03 11:58:08 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -1,14 +1,34 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
-        int round = 0;
-        while(a != b)
-        {
-            a = a/2 + a%2;
-            b = b/2 + b%2;
-            round++;
+import java.util.*;
+class Solution {
+    static int answer = 0;
+    private static void subdivision(int a, int b, int left, int right, int cnt) {
+        if(left == right) return;
+        int mid = (left + right) / 2;
+        
+        boolean[] check = new boolean[2];
+        if(left <= a && mid >= a) check[0] = true;
+        if(mid + 1 <= b && right >= b) check[1] = true;
+        
+        if(check[0] && check[1]) {
+            answer = cnt;
+            return;
         }
-        return round;
+        else if(check[0])
+            subdivision(a, b, left, mid, cnt - 1);
+        else
+            subdivision(a, b, mid + 1, right, cnt - 1);
+    }
+    
+    public static int solution(int n, int a, int b) {
+        int cnt = 0;
+        for(int i = 1; i <= n; i++) {
+            if((int)Math.pow(2, i) == n) {
+                cnt = i;
+                break;
+            }
+        }
+        
+        subdivision(Math.min(a, b), Math.max(a,b), 1, n, cnt);
+        return answer;
     }
 }
