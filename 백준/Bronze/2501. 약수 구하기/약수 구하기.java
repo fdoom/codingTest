d72b80b24d12e2c8239a227a918ef04db7921f1a
fdoom <44052861+fdoom@users.noreply.github.com>
2023-08-05 22:48:14 +0900

    [Bronze III] Title: 약수 구하기, Time: 216 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		int answer = 0;
+		int cnt = 0;
+		for(int i = 1; i <= N; i++) {
+			if(N % i == 0)
+				cnt++;
+			if(cnt == K) {
+				answer = i;
+				break;
+			}
+		}
+		System.out.println(answer);
+	}
+}
