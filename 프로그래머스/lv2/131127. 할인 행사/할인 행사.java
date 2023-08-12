58671b9b0ed108dc61743b3c55a1b35ded1577fa
fdoom <44052861+fdoom@users.noreply.github.com>
2023-08-12 14:19:24 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -8,18 +8,26 @@ class Solution {
         for(int i = 0; i <= discount.length - 10; i++) {
             Map <String, Integer> temp = new HashMap<>();
             temp.putAll(map);
-            
-            for(int j = i; j < i + 10; j++)
-                if(temp.containsKey(discount[j]))
-                    temp.put(discount[j], temp.get(discount[j]) + 1);
-            
             boolean flag = true;
-            for(int j = 0; j < want.length; j++)
-                if(temp.get(want[j]) != number[j])
+            for(int j = i; j < i + 10; j++) {
+                if(!temp.containsKey(discount[j])) {
                     flag = false;
-            if(flag) answer++;
-            
-            
+                    break;
+                }
+                temp.put(discount[j], temp.get(discount[j]) + 1);
+            }
+
+            if(!flag) continue;
+
+            for(int j = 0; j < want.length; j++) {
+                if(temp.get(want[j]) != number[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(!flag) continue;
+
+            answer++;
         }
         return answer;
     }
