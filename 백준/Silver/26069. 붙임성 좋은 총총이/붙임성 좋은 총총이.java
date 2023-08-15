54fa9f45b38681333788d7b28d9bfcd61bbe0c5f
fdoom <44052861+fdoom@users.noreply.github.com>
2023-08-15 09:09:13 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 268 ms, Memory: 19468 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -3,12 +3,13 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Set <String> set = new HashSet<>();
+		set.add("ChongChong");
 		int N = sc.nextInt();
 		while(N-- > 0) {
 			String A = sc.next();
 			String B = sc.next();
 			
-			if((A.equals("ChongChong") || B.equals("ChongChong")) || (set.contains(A) || set.contains(B))) {
+			if(set.contains(A) || set.contains(B)) {
 				set.add(A);
 				set.add(B);
 			}
