63cea390fe1015bf43d2499167383c2241ea9492
fdoom <44052861+fdoom@users.noreply.github.com>
2023-08-18 09:42:24 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    static int answer = 0;
+    static boolean[] check;
+    public int solution(int k, int[][] dungeons) {
+        check = new boolean[dungeons.length];
+        DFS(k, dungeons, 0);
+        return answer;
+    }
+    
+    public void DFS(int k, int[][]d, int deep) {
+        answer = Math.max(answer, deep);
+        for(int i = 0; i < d.length; i++) {
+            if(!check[i] && k >= d[i][0]) {
+                check[i] = true;
+                DFS(k - d[i][1], d, deep + 1);
+                check[i] = false;
+            }
+        }
+    }
+}
