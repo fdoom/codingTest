1286c1defebde8f43695db2138de71584ed931d4
fdoom <44052861+fdoom@users.noreply.github.com>
2023-08-20 16:45:11 +0900

    [level 2] Title: 타겟 넘버, Time: 0.36 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,18 @@
 import java.util.*;
 class Solution {
-    static Set<List<Integer>> set = new HashSet<>();
-    
+    static int answer = 0;
     public int solution(int[] numbers, int target) {
-        List<Integer> list = new ArrayList<>();
-        DFS(numbers, target, 0, 0, list);
-        return set.size();
+        DFS(numbers, target, 0, 0);
+        return answer;
     }
     
-    public static void DFS(int[] numbers, int target, int sum, int deep, List<Integer> list) {
+    public static void DFS(int[] numbers, int target, int sum, int deep) {
         if (deep == numbers.length) {
-            if (sum == target) {
-                set.add(list);
-            }
+            if (sum == target) answer++;
             return;
         }
-
-        list.add(numbers[deep]);
-        DFS(numbers, target, sum + numbers[deep], deep + 1, list);
-        list.remove(deep);
-
-        list.add(numbers[deep] * -1);
-        DFS(numbers, target, sum - numbers[deep], deep + 1, list);
-        list.remove(deep);
+        
+        DFS(numbers, target, sum + numbers[deep], deep + 1);
+        DFS(numbers, target, sum - numbers[deep], deep + 1);
     }
 }
