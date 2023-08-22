6a1651938f87164a20cd0c00e69ee8a91b350d3f
fdoom <44052861+fdoom@users.noreply.github.com>
2023-08-22 15:18:20 +0900

    [level 3] Title: 정수 삼각형, Time: 5.75 ms, Memory: 59.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -4,9 +4,9 @@ class Solution {
         arr = new int[triangle.length][triangle[triangle.length - 1].length];
         return DP(triangle, 0, 0);
     }
-    public static int DP(int[][] triangle, int col, int row) {
-        if(col == triangle.length - 1) return triangle[col][row];
-        else if(arr[col][row] > 0) return arr[col][row];
-        else return arr[col][row] = triangle[col][row] + Math.max(DP(triangle, col + 1, row), DP(triangle, col + 1, row + 1));
+    public static int DP(int[][] triangle, int row, int col) {
+        if(row == triangle.length - 1) return triangle[row][col];
+        else if(arr[row][col] > 0) return arr[row][col];
+        else return arr[row][col] = triangle[row][col] + Math.max(DP(triangle, row + 1, col), DP(triangle, row + 1, col + 1));
     }
 }
