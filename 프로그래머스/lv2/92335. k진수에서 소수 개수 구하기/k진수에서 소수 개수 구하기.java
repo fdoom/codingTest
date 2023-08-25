a32f7df946d794ed6f0840ae375a33337bf6fb9e
fdoom <44052861+fdoom@users.noreply.github.com>
2023-08-25 11:40:12 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 1.10 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.math.*;
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
@@ -9,26 +9,10 @@ class Solution {
         }
         
         for(String s : sb.reverse().toString().split("0")) {
-            long num = 0;
             if(s.isEmpty()) continue;
-            
-            num = Long.parseLong(s);
-            if(num > 1) {
+            BigInteger num = new BigInteger(s);
+            if(num.isProbablePrime(5))
                 answer++;
-                boolean check = false;
-                Set <Long> set = new HashSet<>();
-                for(long i = 2; i * i <= num; i++) {
-                    if(set.contains(i)) continue;
-                    for(long j = i; j <= num; j *= i) {
-                        set.add(j);
-                        if(num % j == 0) {
-                            check = true;
-                            break;
-                        }
-                    }
-                }
-                if(check) answer--;
-            }
         }
         return answer;
     }
