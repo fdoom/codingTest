017ca838e137f6f7026c658d131d6acb6eff86ef
fdoom <44052861+fdoom@users.noreply.github.com>
2023-08-26 06:08:49 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,23 +1,22 @@
 import java.util.*;
 class Solution {
-    static boolean[] check;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        check = new boolean[n];
+        boolean[] check = new boolean[n];
         for(int i = 0; i < n; i++) {
             if(check[i]) continue;
-            DFS(n, computers, i);
+            DFS(n, computers, check, i);
             answer++;
         }
         return answer;
     }
     
-    void DFS(int n, int[][] computers, int index) {
-        if(index >= n) return;
+    private void DFS(int n, int[][] computers, boolean[] check, int index) {
+        check[index] = true;
         for(int i = 0; i < n; i++) {
             if(!check[i] && computers[index][i] == 1) {
                 check[i] = true;
-                DFS(n, computers, i);
+                DFS(n, computers, check, i);
             }
         }
     }
