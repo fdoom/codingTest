daff96b3e7c7a8f66690f5d77682729f9ebfab2e
fdoom <44052861+fdoom@users.noreply.github.com>
2023-08-28 09:39:27 +0900

    [level 3] Title: 최고의 집합, Time: 0.02 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -2,10 +2,10 @@ class Solution {
     public int[] solution(int n, int s) {
         if(n > s) return new int[] {-1};
         int[] answer = new int[n];
-        int idx = 0;
+        int index = 0;
         while(n > 0){
             int value = s/n;
-            answer[idx++] = value;
+            answer[index++] = value;
             s -= value;
             n--;
         }
