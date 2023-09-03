e242039a221ef3c0fa3293c4056ac474803146b1
fdoom <44052861+fdoom@users.noreply.github.com>
2023-09-04 08:55:04 +0900

    [level 2] Title: 모음 사전, Time: 0.73 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -4,21 +4,21 @@ class Solution {
     public int solution(String word) {
         char[] vowel = {'A', 'E', 'I', 'O', 'U'};
         StringBuilder sb = new StringBuilder();
-        sol(word, vowel, sb);
+        dfs(word, vowel, sb);
         return answer;
     }
     
-    public static void sol(String word, char[] vowel, StringBuilder sb) {
+    public static void dfs(String word, char[] vowel, StringBuilder sb) {
         if(sb.toString().length() >= 5 || answer > 0)
             return;
         for(int i = 0; i < vowel.length && answer == 0; i++) {
             sb.append(vowel[i]);
             if(answer == 0 && word.equals(sb.toString())) {
                 answer = cnt;
-                break;
+                break;  
             }
             cnt++;
-            sol(word, vowel, sb);
+            dfs(word, vowel, sb);
             sb.deleteCharAt(sb.toString().length() - 1);
         }
     }
