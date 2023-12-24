a3e8682b09e792e812d80772c2844702f376fc3a
fdoom <44052861+fdoom@users.noreply.github.com>
2023-12-24 22:09:29 +0900

    Time: 14 ms (42.09%), Space: 44.9 MB (11.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str = s.toLowerCase().replaceAll("[^0-9a-zA-Z]", "");
+        String str = s.toLowerCase().replaceAll("[^0-9a-z]", "");
         boolean check = true;
         for(int i = 0; i < str.length() / 2; i++) {
             if(str.charAt(i) != str.charAt(str.length() - i - 1)) {
