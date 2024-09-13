e7bb6246d4467241cfd4254e18fceed1e523d66d
fdoom <44052861+fdoom@users.noreply.github.com>
2024-09-13 18:00:59 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
+        
         for(String[] c : clothes) {
             if(!map.containsKey(c[1]))
                 map.put(c[1], 1);
