8fd0ab06381578babd37ea7d5907ba584ed67859
fdoom <44052861+fdoom@users.noreply.github.com>
2024-09-20 15:37:03 +0900

    [level 2] Title: 기능개발, Time: 10.11 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue <Integer> progressesQueue = Arrays.stream(progresses)
+            .boxed()
+            .collect(Collectors.toCollection(LinkedList::new));
+        
+        Queue <Integer> speedsQueue = Arrays.stream(speeds)
+            .boxed()
+            .collect(Collectors.toCollection(LinkedList::new));
+        
+        Queue<Integer> answer = new LinkedList<>();
+        
+        while (!progressesQueue.isEmpty()) {
+            for (int i = 0; i < progressesQueue.size(); i++) {
+                int updatedProgress = progressesQueue.remove() + speedsQueue.peek();
+                progressesQueue.add(updatedProgress);
+                speedsQueue.add(speedsQueue.remove());
+            }
+
+            int count = 0;
+            while (!progressesQueue.isEmpty() && progressesQueue.peek() >= 100) {
+                progressesQueue.remove();
+                speedsQueue.remove();
+                count++;
+            }
+
+            if (count > 0)
+                answer.add(count);
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
