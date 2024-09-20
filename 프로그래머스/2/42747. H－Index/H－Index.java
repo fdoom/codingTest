021748602924337d5397c884aa5b81e20544f873
fdoom <44052861+fdoom@users.noreply.github.com>
2024-09-20 11:53:22 +0900

    [level 2] Title: H-Index, Time: 10.54 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,16 +2,19 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
-        
+
         int answer = 0;
         for(int i = 0; i <= citations[citations.length - 1]; i++) {
             int cnt = 0;
-            for(int j = 0; j < citations.length; j++)
+            for(int j = 0; j < citations.length; j++) {
                 if(i <= citations[j])
                     cnt++;
-            
+            }
+
             if(cnt >= i)
                 answer = i;
+            else
+                break;
         }
         return answer;
     }
