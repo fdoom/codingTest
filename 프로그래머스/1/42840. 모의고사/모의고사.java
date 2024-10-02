59f35a7e5797256fe729dec6c83fde5fc48cb4b0
fdoom <44052861+fdoom@users.noreply.github.com>
2024-10-03 08:53:51 +0900

    [level 1] Title: 모의고사, Time: 4.28 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -7,20 +7,20 @@ class Solution {
             { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
         };
         
-        int[] cnt = new int[3];
+        int[] score = new int[3];
         int max = 0;
         for(int i = 0; i < answers.length; i++) {
             for(int j = 0; j < p.length; j++) {
                 if(p[j][i % p[j].length] == answers[i]) {
-                    cnt[j]++;
-                    max = Math.max(cnt[j], max);
+                    score[j]++;
+                    max = Math.max(score[j], max);
                 }
             }
         }
         
         List<Integer> answer = new ArrayList<>();
-        for(int i = 0; i < cnt.length; i++) {
-            if(cnt[i] == max) {
+        for(int i = 0; i < score.length; i++) {
+            if(score[i] == max) {
                 answer.add(i + 1);
             }
         }
