fe1ea496a975d7f6d19cc5437609f8273de7bc19
fdoom <44052861+fdoom@users.noreply.github.com>
2024-11-18 14:35:45 +0900

    [level 2] Title: 소수 찾기, Time: 10.77 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
 
 ### 성능 요약
 
-메모리: 86.7 MB, 시간: 26.89 ms
+메모리: 78.9 MB, 시간: 10.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 08일 15:26:27
+2024년 11월 18일 14:35:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,27 +1,39 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
     Set<Integer> primes = new HashSet<>();
-    
+
     public int solution(String numbers) {
-        generateNumbers(numbers, "");
+        boolean[] visited = new boolean[numbers.length()];
+        generateNumbers(numbers, visited, new StringBuilder());
         return primes.size();
     }
 
-    private void generateNumbers(String numbers, String current) {
-        if (!current.isEmpty()) {
-            int num = Integer.parseInt(current);
-            
-            if(isPrime(num))
-               primes.add(num);
+    private void generateNumbers(String numbers, boolean[] visited, StringBuilder current) {
+        // 숫자가 비어 있지 않을 때만 처리
+        if (current.length() > 0) {
+            int num = Integer.parseInt(current.toString());
+
+            // 소수이면 Set에 추가
+            if (isPrime(num)) {
+                primes.add(num);
+            }
         }
 
+        // 모든 숫자를 한번씩 사용하여 가능한 조합 생성
         for (int i = 0; i < numbers.length(); i++) {
-            generateNumbers(
-                numbers.substring(0, i) + numbers.substring(i + 1),
-                current + numbers.charAt(i)
-            );
+            if (!visited[i]) {
+                // 현재 숫자를 추가
+                visited[i] = true;
+                current.append(numbers.charAt(i));
+
+                // 재귀 호출
+                generateNumbers(numbers, visited, current);
+
+                // 백트래킹
+                visited[i] = false;
+                current.deleteCharAt(current.length() - 1);
+            }
         }
     }
 
