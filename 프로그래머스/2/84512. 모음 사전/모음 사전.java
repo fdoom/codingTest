dd1488e7b77a1067b4c9a7b0758902a398b9807d
fdoom <44052861+fdoom@users.noreply.github.com>
2024-12-04 11:28:05 +0900

    [level 2] Title: 모음 사전, Time: 25.31 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,32 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     int answer = 0;
     int cnt = 0;
     char[] v = {'A', 'E', 'I', 'O', 'U'};
     
     public int solution(String word) {
-        dfs(word, new StringBuilder());
+        dfs(word, new Stack<Character>());
         return answer;
     }
     
-    void dfs(String w, StringBuilder sb) {
-        if(sb.toString().length() >= 5) return;
+    void dfs(String w, Stack s) {
+        if(s.size() >= 5) return;
         
         for(int i = 0; i < v.length && answer == 0; i++) {
-            sb.append(v[i]);
+            s.push(v[i]);
             
             cnt++;
-            if(w.equals(sb.toString())) {
+            if(w.equals(s.stream()
+                        .map(String::valueOf)
+                        .collect(Collectors.joining()))) {
                 answer = cnt;
                 break;
             }
             
-            dfs(w, sb);
-            sb.deleteCharAt(sb.toString().length() - 1);
+            dfs(w, s);
+            s.pop();
         }
     }
 }
