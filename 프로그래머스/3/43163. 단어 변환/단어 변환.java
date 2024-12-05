9156080b9ec5f3f5abcd4b7648a182bb46c5a135
fdoom <44052861+fdoom@users.noreply.github.com>
2024-12-05 11:52:38 +0900

    [level 3] Title: 단어 변환, Time: 0.54 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,34 @@
+class Solution {
+    int answer = Integer.MAX_VALUE;
+    
+    public int solution(String begin, String target, String[] words) {
+        boolean[] visited = new boolean[words.length];
+        dfs(begin, target, words, visited, 0);
+        return answer == Integer.MAX_VALUE ? 0 : answer;
+    }
+    
+    void dfs(String b, String t, String[] w, boolean[] v, int cnt) {
+        if(b.equals(t)) {
+            answer = Math.min(answer, cnt);
+            return;
+        }
+        
+        for(int i = 0; i < w.length; i++) {
+            if(canTransform(b, w[i]) == true && !v[i]) {
+                v[i] = true;
+                dfs(w[i], t, w, v, cnt + 1);
+                v[i] = false;
+            }
+        }
+    }
+    
+    boolean canTransform(String a, String b) {
+        int cnt = 0;
+        for(int i = 0; i < a.length(); i++) {
+            if(a.charAt(i) != b.charAt(i)) {
+                cnt++;
+            }
+        }
+        return cnt == 1 ? true : false;
+    }
+}
