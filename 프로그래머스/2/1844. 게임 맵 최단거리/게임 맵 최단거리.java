0c8d95f6a4c357ea1d17bb9b0525145dce2e7361
fdoom <44052861+fdoom@users.noreply.github.com>
2024-12-05 10:43:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.03 ms, Memory: 55 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,52 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int x, y, cnt;
+
+    Node(int x, int y, int cnt) {
+        this.x = x;
+        this.y = y;
+        this.cnt = cnt;
+    }
+}
+
+class Solution {
+    // 위, 아래, 오른쪽, 왼쪽
+    int[][] move = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int answer = -1;
+
+    public int solution(int[][] maps) {
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        visited[0][0] = true;
+        bfs(maps, 0, 0, visited);
+        return answer;
+    }
+
+    void bfs(int[][] maps, int x, int y, boolean[][] v) {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x, y, 1));
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            if (cur.x == maps.length - 1 && cur.y == maps[0].length - 1) {
+                if (answer == -1) answer = cur.cnt;
+                answer = Math.min(answer, cur.cnt);
+                return;
+            }
+
+            for (int[] m : move) {
+                int moveX = cur.x + m[0];
+                int moveY = cur.y + m[1];
+                if (moveX >= 0 && moveX < maps.length &&
+                        moveY >= 0 && moveY < maps[0].length &&
+                        maps[moveX][moveY] == 1 && !v[moveX][moveY]) {
+                    v[moveX][moveY] = true;
+                    q.offer(new Node(moveX, moveY, cur.cnt + 1));
+                }
+            }
+        }
+    }
+
+}
